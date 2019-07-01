2d2f3cc3620238152ecbb80096fde9d1ede25f16
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2019-07-01 21:38:15 +0200

    Add to_make

--- framework.sh
+++ framework.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 #framework.sh - create video makefile from configuration
 
+# FUNCTIONS -------------------------------------------------------------------
+to_make() {
+    printf "$@" >>"$makefile"
+}
+
+# MAIN ------------------------------------------------------------------------
 [ "$#" = 4 ] || { echo "Usage: $0 width height fps length"; exit 1; }
 w="$1"
 h="$2"
@@ -11,12 +17,10 @@ numfmt="%0$(echo "length($length)" | bc)d"
 makefile="Makefile.$cfgfmt"
 
 :>"$makefile"
-printf "output.mp4: " >>"$makefile"
+to_make "output.mp4: "
 for i in $(seq 0 "$length"); do
-    printf "${cfgfmt}_$(printf "$numfmt" "$i").png " >>"$makefile"
+    to_make "${cfgfmt}_$(printf "$numfmt" "$i").png "
 done
-printf "\n" >>"$makefile"
-printf "\tffmpeg -r "$fps" -i \"${cfgfmt}_%${numfmt}.png\" output.mp4" \
-    >>"$makefile"
-
+to_make "\n"
+to_make "\tffmpeg -r "$fps" -i \"${cfgfmt}_%${numfmt}.png\" output.mp4"
 
