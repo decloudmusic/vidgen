d56ec56eecb20d20b5c79707d6038ef2e4297a59
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2019-07-01 21:50:42 +0200

    Add frame coding

--- framework.sh
+++ framework.sh
@@ -6,6 +6,10 @@ to_make() {
     printf "$@" >>"$makefile"
 }
 
+timecode() { #1: frame 2: fps, numfmt
+    printf "$numfmt" "$(echo "$1/$2"|bc)" "$(echo "$1%$2"|bc)"
+}
+
 # MAIN ------------------------------------------------------------------------
 [ "$#" = 4 ] || { echo "Usage: $0 width height fps length"; exit 1; }
 w="$1"
@@ -13,14 +17,14 @@ h="$2"
 fps="$3"
 length="$4"
 cfgfmt="${w}x${h}p${3}"
-numfmt="%0$(echo "length($length)" | bc)d"
+numfmt="%0$(echo "length($length/$fps)"|bc)d.%0$(echo "length($fps)"|bc)d"
 makefile="Makefile.$cfgfmt"
 
 :>"$makefile"
 to_make "output.mp4: "
 for i in $(seq 0 "$length"); do
-    to_make "${cfgfmt}_$(printf "$numfmt" "$i").png "
+    to_make "${cfgfmt}_$(timecode "$i" "$fps").png "
 done
 to_make "\n"
-to_make "\tffmpeg -r "$fps" -i \"${cfgfmt}_%${numfmt}.png\" output.mp4"
+to_make "\tffmpeg -framerate $fps -y -pattern_type glob -i \"${cfgfmt}_*.png\" ${format}.mp4\n"
 
